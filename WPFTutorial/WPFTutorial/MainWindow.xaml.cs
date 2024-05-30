0edab17ab4f228f6fb0966a62804c4bf1a5dbea0
Joshua C <contact.cjoshua@gmail.com>
2024-05-30 14:17:20 -0600

    tutorial part 2

--- WPFTutorial/WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/WPFTutorial/MainWindow.xaml.cs
@@ -2,12 +2,37 @@
 
 namespace WPFTutorial
 {
-    
+
     public partial class MainWindow : Window
     {
+
+        bool running = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (running)
+            {
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+            else
+            {
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+
+
+
+            running = !running;
+
         }
     }
 }
